3cf7d53c826cd51060177b60d5bb9dbd75828715
Kiwe <kiwe0410@gmail.com>
2022-02-12 16:04:32 +0800

    讀取檔案

--- products.py
+++ products.py
@@ -2,11 +2,13 @@
 products=[]
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
+        if '商品, 價格' in line: # 忽略商品價個的if 寫法
+            continue # 跳出這次的回圈, 繼續同個迴圈的下個迴圈
         name, price = line.strip().split(',') #換行(strip)+切割(split),
         products.append([name, price])
 print(products)
 
-
+# 讓使用者輸入
 while True:
   name = input("請輸入商品名稱: ")
   if name == 'q':
@@ -14,10 +16,12 @@ while True:
   price = input("請輸入商品價格: ")
   products.append([name, price])
 print(products)
- 
+
+# 印出所有購買紀錄 
 for p in products:
      print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f: #打開檔案
     f.write('商品, 價格\n')
     for p in products:
